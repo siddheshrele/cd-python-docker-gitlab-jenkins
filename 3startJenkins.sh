c176d2ee8a583ae658b65c0623a68178b32069dc
siddhesh rele <32329791+siddheshrele@users.noreply.github.com>
2020-04-15 13:50:25 +0530

    Update 3startJenkins.sh

--- 3startJenkins.sh
+++ 3startJenkins.sh
@@ -2,7 +2,7 @@
 #Created by siddhesh rele email id:- sidluckie@gmail.com
 #Creats docker containor of jenkins
 
-JENKINS_PORT=8090
+JENKINS_PORT=8080
 JENKINS_CONTAINER_NAME=jenkins
 JENKINS_HOME=~/jenkins_home
 
@@ -11,9 +11,9 @@ mkdir $JENKINS_HOME
 JenkinsContainerId=`docker ps -qa --filter "name=$JENKINS_CONTAINER_NAME"`
 if [ -n "$JenkinsContainerId" ]
 then
-	echo "Stopping and removing existing jenkins container"
-	docker stop $JENKINS_CONTAINER_NAME
-	docker rm $JENKINS_CONTAINER_NAME
+        echo "Stopping and removing existing jenkins container"
+        docker stop $JENKINS_CONTAINER_NAME
+        docker rm $JENKINS_CONTAINER_NAME
 fi
 
 echo "Starting jenkins container on port $JENKINS_PORT and jenkins home is $JENKINS_HOME"
@@ -21,17 +21,19 @@ echo "Starting jenkins container on port $JENKINS_PORT and jenkins home is $JENK
 # https://hub.docker.com/r/jenkinsci/jenkins/tags/
 # /var/jenkins_home contains all plugins and configuration
 docker run -d --name $JENKINS_CONTAINER_NAME \
-	-p $JENKINS_PORT:8080 -p 50000:50000 \
-	-v $JENKINS_HOME:/var/jenkins_home \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v $(which docker):/bin/docker \
-	-u root \
-	jenkins
+        --net=host \
+        -p $JENKINS_PORT:8080 -p 50000:50000 \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $(which docker):/bin/docker \
+        -v $JENKINS_HOME:/var/jenkins_home \
+        -u root \
+        jenkins/jenkins:latest
 
+#alpine image jenkinsci/jenkins:alpine 
+#alpine not used due to jenkins is outdated
+#Install docker and other utility inside the server
+#docker exec -it jenkins apk --update add git less docker
 #Install require uitlity in jenkins containor
 
-docker exec -it jenkins apt-get update 
+docker exec -it jenkins apt-get update
 docker exec -it jenkins apt-get install -y libltdl7
-
-
-
